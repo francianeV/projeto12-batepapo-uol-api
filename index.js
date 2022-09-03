a6031ff8d64702c6686819b1c6bd5c61a8b1cb7a
Franciane <franciane.penina@gmail.com>
2022-09-03 15:04:43 -0300

    feat: add messages POST route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import joi from  'joi';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import dayjs from 'dayjs';
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,12 @@ app.use(express.json());
 
 const userSchema = joi.object({
     name: joi.string().required()
+});
+
+const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
 });
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
@@ -54,6 +61,35 @@ app.get("/participants", async (req, res) => {
     }catch(error){
         return res.sendStatus(500);    }
 })
+
+app.post("/messages", async (req, res) => {
+    const {to, text, type} = req.body;
+    const {user} = req.headers;
+
+    const validation = messagesSchema.validate({to, text, type},{abortEarly: false});
+
+    if(validation.error){
+        return res.sendStatus(422);
+    }
+
+    try {
+        const toUser = await db.collection("participants").findOne({name: user});
+
+        if(!toUser){
+            return res.sendStatus(422);
+        }
+
+        await db.collection("messages").insertOne({from: user, 
+                                                   to: to, 
+                                                   text: text, 
+                                                   type: type, 
+                                                   time: dayjs().format('HH:mm:ss')});
+        return res.sendStatus(201);
+        
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
 
 
 app.listen(5000, () => console.log('listening on port 5000'));
